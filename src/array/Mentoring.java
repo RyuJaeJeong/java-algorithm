eb3c8572d7b4535786dad645ea1fe6982a954e18
financeRyu <fb0982@naver.com>
2025-08-20 16:48:45 +0900

    fix: 12. 멘토링 * 강사 풀의 추가 2025-08-20 류재정

--- src/array/Mentoring.java
+++ src/array/Mentoring.java
@@ -16,14 +16,14 @@ public class Mentoring {
             }
         }
         Mentoring mentoring = new Mentoring();
-        int available = mentoring.solution(N, M, arr);
+        int available = mentoring.solution2(N, M, arr);
         System.out.println(available);
     }
 
     /**
      * 학생 등수배열을 받아 멘토 조합을 반환
-     * 4, 3
      * [input]
+     * 4, 3
      * 3 4 1 2
      * 4 3 2 1
      * 3 1 4 2
@@ -69,6 +69,29 @@ public class Mentoring {
             }
         }
 
+        return answer;
+    }
+
+    /**
+     * 강사 풀의
+     */
+    public int solution2(int N, int M, int[][] arr){
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {          //가능한 조합 1 ~ N번 학생 '이'
+            for (int j = 1; j <= N; j++) {      //가능한 조합 1 ~ N번 학생 '과'
+                if(i == j) continue;
+                int cnt = 0;
+                for (int k = 0; k < M; k++) {
+                    int pi = 0, pj = 0;
+                    for (int l = 1; l < N+1; l++) {
+                        if(arr[k][l] == i) pi = l;
+                        if(arr[k][l] == j) pj = l;
+                    }
+                    if(pi < pj) cnt ++;
+                }
+                if(cnt == M) answer++;
+            }
+        }
         return answer;
     }
 
