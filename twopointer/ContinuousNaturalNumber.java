a13310d5e170159b4db91df13fa2f2d608264462
financeRyu <fb0982@naver.com>
2025-08-25 21:58:23 +0900

    feat: 5. 연속된 자연수의 합 * 선생님 풀의 추가 2025-08-25 류재정

--- twopointer/ContinuousNaturalNumber.java
+++ twopointer/ContinuousNaturalNumber.java
@@ -1,6 +1,8 @@
 package twopointer;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class ContinuousNaturalNumber {
 
@@ -8,7 +10,7 @@ public class ContinuousNaturalNumber {
         Scanner scanner = new Scanner(System.in);
         int targetNumber = scanner.nextInt();
         ContinuousNaturalNumber obj = new ContinuousNaturalNumber();
-        System.out.println(obj.getCount(targetNumber));
+        System.out.println(obj.getCount2(targetNumber));
     }
 
     /**
@@ -42,4 +44,34 @@ public class ContinuousNaturalNumber {
         }
         return answer;
     }
+
+    /**
+     * 연속된 자연수의 합이 N이 된다? >>> 포인터가 targetNumber / 2  + 1 까지만 해보면 된다.
+     * @param targetNumber
+     * @return
+     */
+    public int getCount2(int targetNumber){
+        int[] arr = IntStream.range(1, targetNumber+1).toArray();
+        int lt = 0, rt = 1;
+        int sum = 0;
+        int answer = 0;
+        while(lt < targetNumber / 2 + 1){
+            if(rt == lt + 1){
+                sum = arr[lt] + arr[rt++];
+            }else{
+                sum+=arr[rt++];
+            }
+
+            if(sum == targetNumber){
+                answer++;
+                lt++;
+                rt = lt+1;
+            }else if(sum > targetNumber){
+                lt++;
+                rt = lt+1;
+            }
+        }
+        return answer;
+    }
+
 }
