9a578361053bc42785d0d9b42197ff4d3e5c39a2
financeRyu <fb0982@naver.com>
2025-09-03 23:20:37 +0900

    feat 2: 아나그램(해쉬) * 선생님 풀이 추가 2025-09-02 류재정

--- hashmap/Anagram.java
+++ hashmap/Anagram.java
@@ -9,7 +9,7 @@ public class Anagram {
         String str1 = scanner.nextLine();
         String str2 = scanner.nextLine();
         Anagram anagram = new Anagram();
-        boolean isAnagram = anagram.isAnagram(str1, str2);
+        boolean isAnagram = anagram.isAnagram2(str1, str2);
         System.out.println((isAnagram)?"YES":"NO");
     }
 
@@ -28,5 +28,26 @@ public class Anagram {
             if(mapForStr1.get(ch) == mapForStr2.getOrDefault(ch, 0)) cnt++;
         }
         return cnt==mapForStr1.size();
+    }
+
+    public boolean isAnagram2(String str1, String str2){
+        boolean result = true;
+        HashMap<Character, Integer> mapForStr1 = new HashMap<>();
+        HashMap<Character, Integer> mapForStr2 = new HashMap<>();
+        for (int i = 0; i < str1.length(); i++) {
+            char ch1 = str1.charAt(i);
+            mapForStr1.put(ch1, mapForStr1.getOrDefault(ch1, 0)+1);
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            char ch2 = str2.charAt(i);
+            if(!mapForStr1.containsKey(ch2) && mapForStr1.getOrDefault(ch2, 0) == 0){
+                result = false;
+                break;
+            }
+            mapForStr1.put(ch2, mapForStr1.get(ch2)-1);
+        }
+
+        return result;
     }
 }
