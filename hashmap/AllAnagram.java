e1932d0764d961eaeb2f4a4cb521555e2d6dcccf
financeRyu <fb0982@naver.com>
2025-09-06 12:03:42 +0900

    feat: equals를 이용한 hashmap 비교로 수정 2025-09-06 류재정

--- hashmap/AllAnagram.java
+++ hashmap/AllAnagram.java
@@ -14,27 +14,27 @@ public class AllAnagram {
         System.out.println(cnt);
     }
 
-    public int solution(String sWord, String tWord){
-        Map<Character, Integer> tWordMap = new HashMap<>();
-        for (int i = 0; i < tWord.length(); i++) {
-            char ch = tWord.charAt(i);
-            tWordMap.put(ch, tWordMap.getOrDefault(ch, 0)+1);
-        }
-
-        int lt = 0;
-        int count = 0;
-        for (int rt = tWord.length()-1; rt < sWord.length(); rt++) {
-            String splittedSword = sWord.substring(lt++, rt+1);
-            int same = 0;
-            for(char key : tWordMap.keySet()){
-                int tWordCnt = tWordMap.get(key);
-                int sWordCnt = getCharacterCount(splittedSword, key);
-                if(sWordCnt == tWordCnt) same++;
-            }
-            if(same == tWordMap.size()) count++;
-        }
-        return count;
-    }
+//    public int solution(String sWord, String tWord){
+//        Map<Character, Integer> tWordMap = new HashMap<>();
+//        for (int i = 0; i < tWord.length(); i++) {
+//            char ch = tWord.charAt(i);
+//            tWordMap.put(ch, tWordMap.getOrDefault(ch, 0)+1);
+//        }
+//
+//        int lt = 0;
+//        int count = 0;
+//        for (int rt = tWord.length()-1; rt < sWord.length(); rt++) {
+//            String splittedSword = sWord.substring(lt++, rt+1);
+//            int same = 0;
+//            for(char key : tWordMap.keySet()){
+//                int tWordCnt = tWordMap.get(key);
+//                int sWordCnt = getCharacterCount(splittedSword, key);
+//                if(sWordCnt == tWordCnt) same++;
+//            }
+//            if(same == tWordMap.size()) count++;
+//        }
+//        return count;
+//    }
 
     public int solution2(String sWord, String tWord){
         Map<Character, Integer> tWordMap = new HashMap<>();
@@ -51,20 +51,15 @@ public class AllAnagram {
             for(char ch : splittedSword.toCharArray()){
                 sWordMap.put(ch, sWordMap.getOrDefault(ch, 0)+1);
             }
-            int sameCnt = 0;
-            for (char key: tWordMap.keySet()){
-                if(tWordMap.get(key) == sWordMap.getOrDefault(key, 0)){
-                    sameCnt++;
-                }
-            }
-            if(sameCnt == tWordMap.size()) count++;
+
+            if(tWordMap.equals(sWordMap)) count++;
         }
         return count;
     }
 
-    public static int getCharacterCount(String word, char c){
-        String removed = word.replaceAll("[^" + c + "]", "");
-        int counter = removed.length();
-        return counter;
-    }
+//    public static int getCharacterCount(String word, char c){
+//        String removed = word.replaceAll("[^" + c + "]", "");
+//        int counter = removed.length();
+//        return counter;
+//    }
 }
