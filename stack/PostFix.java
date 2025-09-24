0b0935e683a878badc9502baa3486eda02575178
financeRyu <fb0982@naver.com>
2025-09-24 20:46:45 +0900

    quiz4: 후위식 연산 2025-09-24 류재정

---
+++ stack/PostFix.java
@@ -0,0 +1,47 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PostFix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PostFix postFix = new PostFix();
+        String expression = sc.nextLine();
+        System.out.println(postFix.cal(expression));
+
+    }
+
+    /**
+     * 후위식 연산을 수행 하여 값을 출력함.
+     * 후위식 연산? 32+ > 3 + 2
+     * 324+* > 3 * (2 + 4)
+     * @param expression 식
+     * @return 계산값
+     */
+    public int cal(String expression){
+        Stack<Integer> stack = new Stack<>();
+        for(char ch: expression.toCharArray()){
+            if(ch == '+'){
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num1+num2);
+            }else if(ch == '-'){
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num2-num1);
+            }else if(ch == '*'){
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num1*num2);
+            }else if(ch == '/'){
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num2/num1);
+            }else{
+                stack.push(Integer.parseInt(ch + ""));
+            }
+        }
+        return stack.pop();
+    }
+}
