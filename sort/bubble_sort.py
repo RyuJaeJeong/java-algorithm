ff1d0a57897878b58dec2ee0e18db229c2963de6
financeRyu <fb0982@naver.com>
2025-10-04 14:56:42 +0900

    algo2: 파이썬 버블 정렬 2025-10-04 류재정

---
+++ sort/bubble_sort.py
@@ -0,0 +1,29 @@
+def swap(arr:list, idx1: int, idx2: int):
+    """
+    idx1과 idx2의 인자를 교환
+    @param arr 교환 대상 배열
+    @param idx1 교환 대상 인덱스1
+    @param idx2 교환 대상 인덱스2
+    """
+    temp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = temp
+
+def sort(arr):
+    """
+    버블 정렬을 수행
+    @param arr 정렬할 배열
+    """
+    length = len(arr)
+    for i in range(length-1):
+        for j in range(length-1, i, -1):
+            if arr[j] < arr[j-1]:
+                swap(arr, j, j-1)    
+                
+
+if __name__=="__main__":
+    length = int(input())
+    arr = input().split(" ")
+    arr = [int(num) for num in arr]
+    sort(arr)
+    print(arr)
